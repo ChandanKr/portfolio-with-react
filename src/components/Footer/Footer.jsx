a012b69d8c76f251a2b3fa8103a71e05bdc584ab
Chandan Kumar <chandan2606kr@gmail.com>
2025-08-31 01:09:32 +0530

    Restored

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,8 @@
-// import React from "react";
-// import "./Footer.css";
-
-// const Footer = () => {
-//   return <div className="footer">Copyright © 2024 Chandan Kumar - All Rights Reserved</div>;
-// };
-
-// export default Footer;
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetch("https://countapi.julien-maury.dev/hit/chandan-kr-portfolio/visits")
-      .then((res) => res.json())
-      .then((data) => setCount(data.value))
-      .catch((err) => console.error("Visitor counter error:", err));
-  }, []);
-
-  return (
-    <div className="footer">
-      Copyright © 2024 Chandan Kumar - All Rights Reserved
-      <br />
-      Visitors Count: {count}
-    </div>
-  );
+  return <div className="footer">Copyright © 2024 Chandan Kumar - All Rights Reserved</div>;
 };
 
 export default Footer;
