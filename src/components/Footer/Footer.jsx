232005d9a76cf7a253f62f102a95747c539011ae
Chandan Kumar <chandan2606kr@gmail.com>
2025-08-31 00:57:51 +0530

    DemoTrial

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+// import React from "react";
+// import "./Footer.css";
+
+// const Footer = () => {
+//   return <div className="footer">Copyright © 2024 Chandan Kumar - All Rights Reserved</div>;
+// };
+
+// export default Footer;
+
+import React, { useEffect, useState } from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <div className="footer">Copyright © 2024 Chandan Kumar - All Rights Reserved</div>;
+  const [count, setCount] = useState(0);
+
+  // useEffect(() => {
+  //   fetch("https://api.getcounter.dev/user/chandan-portfolio")
+  //     .then(res => res.json())
+  //     .then(data => setCount(data.count))
+  //     .catch(err => console.error("Visitor counter error:", err));
+  // }, []);
+
+  useEffect(() => {
+    fetch("https://api.getcounter.dev/user/chandan-portfolio-12345")
+      .then((res) => res.json())
+      .then((data) => setCount(data.count))
+      .catch((err) => console.error("Visitor counter error:", err));
+  }, []);
+
+  return (
+    <div className="footer">
+      Copyright © 2024 Chandan Kumar - All Rights Reserved
+      <br />
+      Visitors Count: {count}
+    </div>
+  );
 };
 
 export default Footer;
